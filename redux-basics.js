5a3b1308379e41aa0dffc741892e873aa60eda14
abhinav7693ngh <abhinav7693ngh@gmail.com>
2019-12-30 00:09:25 +0530

    Redux basics with node

---
+++ redux-basics.js
@@ -0,0 +1,45 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+
+const initialState = {
+    counter : 0
+};
+
+// Reducer
+
+const rootReducer = (state = initialState , action) =>{
+    if(action.type === 'INC_COUNTER'){
+        return {
+            ...state,
+            counter : state.counter + 1
+        };
+    }
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
+    return state;
+};
+
+// Store
+
+const store = createStore(rootReducer);
+
+
+// Subscription
+
+store.subscribe(() => {
+    console.log('State Changed');
+});
+
+
+// Dispatching Action
+
+store.dispatch({type:'INC_COUNTER'});
+store.dispatch({ type: 'ADD_COUNTER',value : 10});
+
+
+
